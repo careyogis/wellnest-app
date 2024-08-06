3236ccab616944b152d8bec7c584deca6802136a
Sankalp <planetsankalp@gmail.com>
2024-08-06 09:32:59 +0530

    Added Styling to Activity view

--- frontend/src/components/TaskAccordian.vue
+++ frontend/src/components/TaskAccordian.vue
@@ -1,74 +1,120 @@
 <template>
-    <div>
-        <!-- Component: Outline accordion -->
-        <section class="w-full bg-white border divide-y rounded divide-slate-200 border-slate-200">
-        <details class="p-4 group" open>
-            <summary class="[&::-webkit-details-marker]:hidden relative pr-8 font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900 ">
-                Blood Pressure
-            <svg xmlns="http://www.w3.org/2000/svg" class="absolute right-0 w-4 h-4 transition duration-300 top-1 stroke-slate-700 shrink-0 group-open:rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5" aria-labelledby="title-ac21 desc-ac21">
-                <title id="title-ac21">Open icon</title>
-                <desc id="desc-ac21">icon that represents the state of the summary</desc>
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
-            </svg>
-            </summary>
-            <p class="mt-4 text-slate-500">
-                <div class="p-8">
-                    <TextInput
-                    :type="'text'"
-                    size="sm"
-                    variant="outline"
-                    placeholder="120/65"
-                    /> 
-                    mm/hg
-                </div>
-                <div>
-                    Notes: <br>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum iste suscipit laudantium, odit sunt ipsa dolorem dolorum beatae voluptatem ratione in, neque ea culpa provident doloribus tempore a facere veritatis.
-                </div>
-                <div>
+  <!-- Component: Outline accordion -->
+  <section
+    class="w-full bg-white border divide-y rounded divide-slate-200 border-slate-200"
+  >
+    <details class="p-3 group" open>
+      <summary
+        class="[&::-webkit-details-marker]:hidden flex justify-between items-center relative font-medium list-none cursor-pointer text-slate-700 focus-visible:outline-none transition-colors duration-300 group-hover:text-slate-900"
+      >
+        <div class="order-2 text-[#070707] font-semibold">Blood Pressure</div>
+        <div class="order-3 flex justify-between items-center">
+          <div>
+            <FeatherIcon
+              class="w-6 mr-1 stroke-[#78abaf] stroke-2"
+              name="clock"
+            />
+          </div>
+          <div class="text-[#070707] font-semibold">10 AM</div>
+        </div>
 
-                </div>
-                    <FileUploader
-                    :fileTypes="['image/*']"
-                    :validateFile="validateFileFunction"
-                    @success="onSuccess"
-                    >
-                        <template #default="{ file, uploading, progress, uploaded, message, error, total, success, openFileSelector }">
-                            <Button
-                            @click="openFileSelector"
-                            :loading="uploading"
-                            >
-                                {{ uploading ? `Uploading ${progress}%` : 'Upload Image' }}
-                            </Button>
-                        </template>
-                    </FileUploader>
-                    <Button
-                    :variant="'outline'"
-                    theme="gray"
-                    size="lg"
-                    label="Button"
-                    >
-                        Mark as Completed
-                    </Button>
-            </p>
-        </details>
-        </section>
-        <!-- End Outlined accordion -->
-         <div>
-
-         </div>
-    </div>
+        <!-- <Button
+            :variant="'ghost'"
+            class="transition duration-300 stroke-slate-700 group-open:rotate-90 w-8 h-8"
+          >
+            <FeatherIcon
+              class="w-5 stroke-[#070707] stroke-2"
+              name="chevron-right"
+            />
+          </Button> -->
+        <FeatherIcon
+          class="transition duration-300 stroke-slate-700 group-open:rotate-90 w-5 stroke-[#070707] stroke-2"
+          name="chevron-right"
+        />
+      </summary>
+      <div class="mt-4 px-7 text-slate-500">
+        <div class="flex">
+          <TextInput
+            :type="'text'"
+            size="lg"
+            variant="outline"
+            placeholder="120/65"
+          />
+          <div class="self-end">mm/hg</div>
+        </div>
+        <FeatherIcon
+          class="inline-block w-3 -rotate-45 mr-1 stroke-[blue] stroke-1"
+          name="paperclip"
+        />
+        <div class="inline-block my-5 text-blue-500">reading1.png</div>
+        <div class="text-[#070707] font-semibold">Notes:</div>
+        <div>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
+          iste suscipit laudantium, odit sunt ipsa dolorem dolorum beatae
+          voluptatem ratione in, neque ea culpa provident doloribus tempore a
+          facere veritatis.
+        </div>
+        <FileUploader
+          class="mt-5"
+          :fileTypes="['image/*']"
+          :validateFile="validateFileFunction"
+          @success="onSuccess"
+        >
+          <template
+            #default="{
+              file,
+              uploading,
+              progress,
+              uploaded,
+              message,
+              error,
+              total,
+              success,
+              openFileSelector,
+            }"
+          >
+            <!-- <Button :variant="'outline'" theme="gray" size="lg" @click="openFileSelector" :loading="uploading">
+                {{ uploading ? `Uploading ${progress}%` : 'Upload Image' }}
+              </Button> -->
+            <button
+              class="text-xl font-medium border-2 border-gray-500 rounded w-full py-2 mb-1"
+              @click="openFileSelector"
+              :loading="updloading"
+            >
+              {{ uploading ? `Uploading ${progress}%` : 'Upload Image' }}
+            </button>
+          </template>
+        </FileUploader>
+        <div class="flex justify-between items-center">
+          <button
+            class="text-xl font-medium border-2 border-gray-500 rounded w-2/3 py-2 mb-2"
+          >
+            Mark as Completed
+            <!-- TODO: Items are not centered properly -->
+          </button>
+          <div class="order-3 flex justify-between items-center">
+              <FeatherIcon
+                class="w-6 mr-1 stroke-[#78abaf] stroke-2"
+                name="clock"
+              />
+            <div class="text-[#070707] font-semibold">10 AM</div>
+          </div>
+        </div>
+        <!-- <Button :variant="'outline'" theme="gray" size="xl" label="Button">
+          Mark as Completed
+        </Button> -->
+      </div>
+    </details>
+  </section>
+  <!-- End Outlined accordion -->
 </template>
 
 <script setup>
-import { TextInput, FileUploader, Button } from 'frappe-ui';
+import { TextInput, FileUploader, Button, FeatherIcon } from 'frappe-ui'
 import { createListResource } from 'frappe-ui'
 
 const validateFileFunction = (fileObject) => {}
 const onSuccess = (file) => {
-    console.log(file)
+  console.log(file)
 }
-
-
 </script>
-
--- frontend/src/pages/Activity.vue
+++ frontend/src/pages/Activity.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
-    <h1 class="text-gray-900 font-bold text-[32px]">Daily Tasks</h1>
-    <div v-if="beneficiary">
-      <BeneficiaryNavbar />
-      <Tabs v-model="state.index" :tabs="state.tabs">
+    <CaregiverNavbar title="Daily Tasks" />
+    <div class="flex flex-col items-center my-3">
+      <!-- TODO: fetch avatar image from backend -->
+      <Avatar
+        :shape="'circle'" 
+        :image="'https://lh5.googleusercontent.com/proxy/gXCFTaGwqD6OXUkJWgLLGw5vAFyJUTnFQjRGF9N_n9dH7alWLedGpd_6mPfAMrJWyVw5fmx_4zMNhUnP-CFnDVe7HLbbWrAQgpgrf7aR32eoZ3euxAX48BrCXtGajHMd'"
+        label="EY"
+        size="3xl"
+      />
+      <div class="text-xl text-[#070707] font-semibold">
+      <!-- TODO: fetch name from backend -->
+        <!-- {{ caregiverResource.data.full_name }} -->
+          Beneficiary Name
+      </div>
+      <div>
+      <!-- TODO: fetch gender, age from backend -->
+        Gender, Age
+      </div>
+    </div>
+      <Tabs class="bro" v-model="state.index" :tabs="state.tabs">
         <template #default="{ tab }">
           <div class="p-5">
             <div>
               <div v-if="state.index === 0">
+                <!-- TODO: fetch this from the backend as well as make this a slot(vue) to take in variables and produce accrordians dynamically -->
+                <TaskAccordian />
                 <TaskAccordian />
               </div>
               <div v-else-if="state.index === 1"></div>
@@ -17,14 +35,13 @@
         </template>
       </Tabs>
     </div>
-  </div>
 </template>
 
 <script setup>
-import { Tabs, FeatherIcon, Badge } from 'frappe-ui'
+import { Tabs, FeatherIcon, Badge, Avatar } from 'frappe-ui'
 import { computed, reactive, ref } from 'vue'
 import { createDocumentResource } from 'frappe-ui'
-import BeneficiaryNavbar from '../components/BeneficiaryNavbar.vue'
+import CaregiverNavbar from '../components/CaregiverNavbar.vue'
 import TaskAccordian from '../components/TaskAccordian.vue'
 
 const state = reactive({
@@ -39,15 +56,4 @@ const state = reactive({
   ],
 })
 
-let beneficiaryResource = createDocumentResource({
-  doctype: 'Beneficiary',
-  name: 'BENF-000017',
-  auto: true,
-})
-
-const beneficiary = computed(() => {
-  if (beneficiaryResource.doc) {
-    return beneficiaryResource.doc
-  }
-})
 </script>
