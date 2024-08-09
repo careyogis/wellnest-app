f8e3e62661073e5d1139be6e1c0028a1e8aa3b75
Sankalp <planetsankalp@gmail.com>
2024-08-09 09:28:34 +0530

    Refactored code and modified styling for Dashboard view

---
+++ frontend/src/components/NursingManager.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="nursingManager mb-7"
+  >
+  <!-- TODO: Static data for now, fetch from backend -->
+    <div class="text-[#070707] font-semibold mb-3">Nursing Manager</div>
+    <div class="mb-3 flex gap-2">
+      <FeatherIcon class="w-5" name="user" />
+      <div class="text-[14px]">
+        Sankalp Srivastava
+        <!-- {{ agencyResource.data[0].agency_name }} -->
+      </div>
+    </div>
+    <div class="mb-3 flex gap-2">
+      <FeatherIcon class="w-4" name="phone" />
+      <div class="text-[14px]">
+        +91 1234567890
+        <!-- {{ agencyResource.data[0].primary_phone.slice(0, 3) }}
+        {{ agencyResource.data[0].primary_phone.slice(4) }} -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { createResource, FeatherIcon, Badge } from 'frappe-ui'
+// TODO: switch api call from agency to customer(Some issue with that doctype, filled as static for now)
+
+// const props = defineProps({
+//   agencyName: String,
+// })
+
+// let agencyResource
+// if (props.agencyName) {
+//   agencyResource = createResource({
+//     url: 'frappe.client.get_list',
+//     params: {
+//       doctype: 'Agency',
+//       fields: ['agency_name', 'primary_phone', 'complete_address'],
+//       name: props.agencyName,
+//     },
+//     auto: true,
+//   })
+// }
+</script>
--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -40,39 +40,63 @@
   </div>
   <div class="w-90%">
     <div class="m-[10px] p-4 bg-white border rounded border-slate-600">
-      <Avatar
-        :shape="'circle'"
-        :image="'https://lh5.googleusercontent.com/proxy/gXCFTaGwqD6OXUkJWgLLGw5vAFyJUTnFQjRGF9N_n9dH7alWLedGpd_6mPfAMrJWyVw5fmx_4zMNhUnP-CFnDVe7HLbbWrAQgpgrf7aR32eoZ3euxAX48BrCXtGajHMd'"
-        label="EY"
-        size="2xl"
-      />
-      <hr />
-      <div class="mb-7">
-        <div class="text-xl text-[#070707] font-semibold mb-3">
-          Contact Information
+      <div class="flex justify-between">
+        <div class="flex items-center gap-1">
+          <Avatar
+            :shape="'circle'"
+            :image="'https://lh5.googleusercontent.com/proxy/gXCFTaGwqD6OXUkJWgLLGw5vAFyJUTnFQjRGF9N_n9dH7alWLedGpd_6mPfAMrJWyVw5fmx_4zMNhUnP-CFnDVe7HLbbWrAQgpgrf7aR32eoZ3euxAX48BrCXtGajHMd'"
+            label="EY"
+            size="2xl"
+          />
+          <div>
+            <div class="font-semibold">
+              {{ caregiverResource.data.full_name }}
+            </div>
+            <!-- calculate age and replace it with the static data -->
+            <div class="">{{ caregiverResource.data.gender }} 23</div>
+          </div>
         </div>
+        <div class="flex">
+          <FeatherIcon
+            class="w-6 mt-0.5 stroke-[#10BAAB] stroke-2 self-start"
+            name="calendar"
+          />
+          <div class="flex flex-col items-center">
+            <div class="font-semibold">15 June 2024 - 25 June 2024</div>
+            <div>5 more days to go</div>
+            <div class="flex gap-1">
+              <FeatherIcon
+                class="w-5 mt-0.5 stroke-[#10BAAB] stroke-2"
+                name="clock"
+              />
+              <div>10 AM - 5 PM</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr class="m-4" />
+      <div class="mb-7">
+        <div class="text-[#070707] font-semibold mb-3">Contact Details</div>
         <div class="mb-3 flex gap-2">
           <FeatherIcon class="w-4" name="phone" />
-          <div>
+          <div class="text-[14px]">
             {{ caregiverResource.data.phone_number.slice(0, 3) }}
             {{ caregiverResource.data.phone_number.slice(4) }}
           </div>
         </div>
         <div class="flex gap-2">
           <FeatherIcon class="w-4 mt-0.5" name="mail" />
-          <div>
+          <div class="text-[14px]">
             {{ caregiverResource.data.email }}
           </div>
         </div>
       </div>
-      <!-- TODO: MAKE ALL THE BELOW INFO DYNAMIC -->
-      <Agency :agencyName="caregiverResource.data.agency" />
+      <Customer />
+      <NursingManager />
       <div class="mySpecialities mb-5">
-        <div class="text-xl text-[#070707] font-semibold mb-2">
-          My Specialities
-        </div>
+        <div class="text-[#070707] font-semibold mb-2">Services Needed</div>
         <div v-if="caregiverResource.data.caregiver_type === 'Attendant'">
-          <!-- make this dynamic for when it's attendant and when it's nursing -->
           <span
             v-for="specialization in caregiverResource.data.attendant_care"
             class="mx-1"
@@ -83,7 +107,6 @@
           </span>
         </div>
         <div v-if="caregiverResource.data.caregiver_type === 'Nurse'">
-          <!-- make this dynamic for when it's attendant and when it's nursing -->
           <span
             v-for="specialization in caregiverResource.data
               .nursing_specialization"
@@ -95,10 +118,44 @@
           </span>
         </div>
       </div>
-      <div class="mb-5">
-        <div class="text-xl text-[#070707] font-semibold mb-2">
-          Important Documents
+      <button
+        class="text-xl font-medium border border-gray-300 rounded w-1/2 py-2 mb-4"
+      >
+        <div class="flex justify-evenly">
+          <FeatherIcon
+            class="w-4 mt-0.5 stroke-[#10BAAB] stroke-2"
+            name="check-square"
+          />
+          <div class="text-[#10BAAB]">Tasks</div>
+          <FeatherIcon
+            class="w-4 mt-0.5 stroke-[#10BAAB] stroke-2"
+            name="chevron-right"
+          />
         </div>
+      </button>
+      <div class="flex gap-6">
+        <button
+          class="text-xl font-medium border-2 border-gray-500 rounded w-full py-2 mb-1"
+        >
+          <div class="flex justify-center gap-1">
+            <FeatherIcon
+              class="w-4 mt-0.5 stroke-gray-500 stroke-2"
+              name="eye"
+            />
+            <div>Checkin</div>
+          </div>
+        </button>
+        <button
+          class="text-xl font-medium border-2 border-gray-500 rounded w-full py-2 mb-1"
+        >
+          <div class="flex justify-center gap-1">
+            <FeatherIcon
+              class="w-4 mt-0.5 stroke-gray-500 stroke-2"
+              name="eye"
+            />
+            <div>Checkout</div>
+          </div>
+        </button>
       </div>
     </div>
   </div>
@@ -109,6 +166,8 @@ import { reactive } from 'vue'
 import { FeatherIcon, Badge, Avatar, createResource } from 'frappe-ui'
 import { session } from '../data/session'
 import Agency from '../components/Agency.vue'
+import Customer from '../components/Customer.vue'
+import NursingManager from '../components/NursingManager.vue'
 
 let caregiverResource = createResource({
   url: 'frappe.client.get',
