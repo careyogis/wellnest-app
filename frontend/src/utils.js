e25a6f03a3b086300f685aa60f9ffee3b2c0fdf9
Sankalp <planetsankalp@gmail.com>
2024-09-27 10:51:44 +0530

    Updated Utility functions which are used in all pages

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -5,11 +5,38 @@ export function formatCurrency(amount, currency) {
   }).format(amount)
 }
 
-export function dateFormatter(date) {
+export function longDateFormatter(date) {
+  let temp = new Date(date)
+  return temp.toLocaleDateString('en-In', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+  })
+}
+
+export function shortDateFormatter(date) {
   let temp = new Date(date)
   return temp.toLocaleDateString('en-In', {
     day: 'numeric',
     month: 'short',
     year: 'numeric',
   })
+}
+
+export function dayFormatter(date) {
+  let temp = new Date(date)
+  return temp.toLocaleDateString('en-In', {
+    weekday: 'long',
+  })
+}
+
+export function getAge(dateString) {
+  let today = new Date()
+  let birthDate = new Date(dateString)
+  let age = today.getFullYear() - birthDate.getFullYear()
+  let m = today.getMonth() - birthDate.getMonth()
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--
+  }
+  return age
 }
