647e91a27f296f14dc40918d23fd623366cd5312
Randhir K Choudhary <randhirk@careyogis.com>
2024-12-10 14:27:11 +0530

    Removed Desk access and Write permission for Caregiver Role on Caregiver doctype.

--- wellnest/hooks.py
+++ wellnest/hooks.py
@@ -228,6 +228,7 @@ app_license = "mit"
 # }
 
 fixtures = ["State", "City", "Specialization", "Attendant Service", "Nursing Service", "Spoken Language", "Medical Condition", "Lead Status", "Item Activity", 
+        {"doctype": "Role", "filters": [["name", "like", "Caregiver%"]]},
             # export only those records that match the filter from Custom Field table
         {"doctype": "Custom Field", "filters": [["module", "like", "WellNest%"]]},
         { "doctype": "Client Script", "filters": [["module", "like", "WellNest%"]] }
