7e22fe4c1c3e8316d330d6afba623f54d00ae460
ForwardZzz <shubhamkumasrivastva4@gmail.com>
2025-05-26 13:22:22 +0530

    Customer Acceptance & Engagement flow/T&C registration

--- wellnest/wellnest/doctype/engagement/engagement.js
+++ wellnest/wellnest/doctype/engagement/engagement.js
@@ -14,3 +14,15 @@ frappe.ui.form.on('Required Activity', {
         frappe.model.set_value(cdt, cdn, 'customer', frm.doc.customer);
     }
 });
+
+frappe.ui.form.on('Engagement', {
+    refresh: function(frm) {
+        if (!frm.doc.__islocal) {
+            frm.add_custom_button('Generate Acceptance Link', () => {
+                const base_url = window.location.origin;
+                const link = `${base_url}/accept-engagement?engagement_id=${frm.doc.name}`;
+                frappe.msgprint(`Share this link: <br><b><a href="${link}" target="_blank">${link}</a></b>`);
+            });
+        }
+    }
+});
