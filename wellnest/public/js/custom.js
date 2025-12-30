85c26619e9bfe34bd49b150024e01fe9bef4be22
Randhir K Choudhary <randhirk@careyogis.com>
2025-12-30 21:12:39 +0530

    Fix: website page and images

--- wellnest/public/js/custom.js
+++ wellnest/public/js/custom.js
@@ -7,9 +7,9 @@
         window.open('https://www.linkedin.com/company/wellnesthomecare/','_blank');
     });
 
-    $('.main-nav-one__cta').on('click', function() {
-        window.open('https://wa.me/+916302693084?text=hi','_blank');
-    });
+    // $('.main-nav-one__cta').on('click', function() {
+    //     window.open('https://wa.me/+916302693084?text=hi','_blank');
+    // });
 
     $('.fa.fa-map-marker-alt').on('click', function() {
         window.open('https://maps.app.goo.gl/TR4bSosJBP8mivDu5','_blank');
