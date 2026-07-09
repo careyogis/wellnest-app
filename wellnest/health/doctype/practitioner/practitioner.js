b94f7ddb69bc7d773e492ae91866d28d89371f6a
Randhir K Choudhary <randhirk@careyogis.com>
2026-07-09 23:02:59 +0530

    Digital Hospital related data models

---
+++ wellnest/health/doctype/practitioner/practitioner.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, www.careyogis.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Practitioner", {
+// 	refresh(frm) {
+
+// 	},
+// });
