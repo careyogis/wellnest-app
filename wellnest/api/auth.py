c046f4e0403011ddf280435717662a9faaf6ed91
Randhir K Choudhary <randhirk@careyogis.com>
2026-08-28 14:25:17 +0530

    minor fix

--- wellnest/api/auth.py
+++ wellnest/api/auth.py
@@ -288,7 +288,6 @@ def register_customer(id_token: str, full_name: str):
 	# Set user in the session so that all other documents get created with this id
 	# This enables us to set permissions for the creator
 	frappe.set_user(user_doc.name)
-	frappe.db.set_user(user_doc.name)
 
 	try:
 		# Now Create Contact, Customer, Patient and Terms Acceptance
