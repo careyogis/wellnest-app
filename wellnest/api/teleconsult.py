94fa7432ecb4a0e7fd1779ba5ccbafa26883e128
Randhir K Choudhary <randhirk@careyogis.com>
2026-09-07 20:48:36 +0530

    changed doctor notification url api method for reporting no_show that also creates a ticket

--- wellnest/api/teleconsult.py
+++ wellnest/api/teleconsult.py
@@ -45,3 +45,29 @@ def book_appointment(practitioner, patient, scheduled_time, consultation_type, c
 	return {                                                                                                                                          
 		"name": appointment.name                                                                                                                      
 	}
+
+@frappe.whitelist()
+def report_doctor_noshow(appointment_id):
+	"""                                         
+	Updates the status of a Patient Appointment to 'No Show' and creates a support ticket for investigating.                                                  
+	"""
+	appointment = frappe.get_doc("Patient Appointment", appointment_id)                     
+	if appointment.status != "Scheduled":
+		frappe.log_error(f"Customer reported 'No Show' for the appointmentId: {appointment_id}, but the status was {appointment.status}")
+		return {"message": f"Appointment in {appointment.status} state cannot be marked as 'No Show'."}
+																																		
+	appointment.status = "No Show"                                                                                                                   
+	appointment.save(ignore_permissions=True)                                                                                                         
+																																		
+	# Create an Issue for the support team to investigate
+	issue = frappe.get_doc({
+		"doctype": "Issue",
+		"subject": f"Customer reported 'No Show' for appointment {appointment_id}",
+		"description": f"The customer reported that the doctor: {appointment.practitioner} did not show up for the appointment with ID {appointment_id}. Please investigate.",
+		"issue_type": "Service",
+		"raised_by": appointment.patient,
+		"assigned_to": appointment.practitioner,
+	})
+	issue.insert(ignore_permissions=True)
+																																					
+	return {"message": "Appointment marked as 'No Show' and the support team have been notified."}
