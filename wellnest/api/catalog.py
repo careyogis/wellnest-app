42b296a03655ece34108d9c9a6af1d5134f57dd2
Randhir K Choudhary <randhirk@careyogis.com>
2026-09-09 08:03:08 +0530

    fix: don't show disabled items in the catalog

--- wellnest/api/catalog.py
+++ wellnest/api/catalog.py
@@ -42,7 +42,7 @@ def get_recommended_packages(parent_group: str = "Packages"):
 	# 3. Fetch all active prices for these items (Removed limit_page_length)
 	prices = frappe.get_all(
 		"Item Price",
-		filters=[["item_code", "in", item_codes]],
+		filters=[["item_code", "in", item_codes], ["disabled", "=", 0]],
 		fields=["item_code", "price_list_rate", "uom", "packing_unit"],
 		ignore_permissions=True,
 	)
